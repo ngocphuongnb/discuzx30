f18873b06794cf5157f988b3b066ccb1434eacb0
vvotintsev <vvotintsev@gmail.com>
2013-03-10 13:36:30 +0400

    PL language copied from x2.5ML

---
+++ upload/source/language/pl/forum/lang_modaction.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_modaction.php by Valery Votintsev at sources.ru
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = array
+(
+
+	'EDT'	=> 'Edytowanych elementów',//'编辑',
+
+	'DEL'	=> 'Usuniętych elementów',//'删除',
+	'DLP'	=> 'Usuniętych odpowiedzi',//'删除回复',
+	'DCM'	=> 'Usuniętych komentarzy',//'删除点评',
+	'PRN'	=> 'Prune posts',//'批量删帖',
+	'UDL'	=> 'Przywróconych',//'反删除',
+
+	'DIG'	=> 'Wykopów',//'加入精华',
+	'UDG'	=> 'Odkopów',//'解除精华',
+	'EDI'	=> 'Limit wykopów',//'限时精华',
+	'UED'	=> 'Unlimit digest',//'解除限时精华',
+
+	'CLS'	=> 'Zamkniętych elementów',//'关闭',
+	'OPN'	=> 'Otwartych elementów',//'打开',
+	'ECL'	=> 'Limit Close',//'限时关闭',
+	'UEC'	=> 'Unlimit close',//'解除限时关闭',
+	'EOP'	=> 'Limit open',//'限时打开',
+	'UEO'	=> 'Unlimit open',//'解除限时打开',
+
+	'STK'	=> 'Przyklej',//'置顶',
+	'UST'	=> 'Odklej',//'解除置顶',
+	'EST'	=> 'Stick Time Limited',//'限时置顶',
+	'UES'	=> 'UnStick Time Limited',//'解除限时置顶',
+
+	'SPL'	=> 'Wydzielonych',//'分割',
+	'MRG'	=> 'Scalonych',//'合并',
+
+	'HLT'	=> 'Wyróżnionych',//'设置高亮',
+	'UHL'	=> 'Usuniętych wyróżnień',//'解除高亮',
+	'EHL'	=> 'Time-limited Highlight',//'限时高亮',
+	'UEH'	=> 'Remove time-limited Highlight',//'解除限时高亮',
+
+	'BMP'	=> 'Odświeżonych',//'提升',
+	'DWN'	=> 'Zestarzonych',//'下沉',
+
+	'MOV'	=> 'Przeniesionych',//'移动',
+	'CPY'	=> 'Skopiowanych',//'复制',
+	'TYP'	=> 'Zmian typów',//'分类',
+
+	'RFD'	=> 'Refund',//'强制退款',
+
+	'MOD'	=> 'Moderacja',//'审核通过',
+
+	'ABL'	=> 'Dodanych blogów',//'加入文集',
+	'RBL'	=> 'Usuniętych blogów',//'移除文集',
+
+	'PTS'	=> 'Przeniesionych do kosza',//'推送主题',
+	'RFS'	=> 'Przywróconych z kosza',//'解除推送',
+	'RMR'	=> 'Usuniętych nagród',//'取消悬赏',
+	'BNP'	=> 'Zablokowanych postów',//'屏蔽帖子',
+	'UBN'	=> 'Odblokowanych postów',//'解除屏蔽',
+
+	'REC'	=> 'Poleconych',//'推荐',
+	'URE'	=> 'Usuniętych poleceń',//'解除推荐',
+
+	'WRN'	=> 'Przyznanych ostrzeżeń',//'警告',
+	'UWN'	=> 'Usuniętych ostrzeżeń',//'解除警告',
+
+	'SPA'	=> 'Dodanych pieczątek',//'添加图章',
+	'SPD'	=> 'Usuniętych pieczątek',//'撤销图章',
+
+	'SLA'	=> 'Dodanych ikonek',//'添加图标',
+	'SLD'	=> 'Usuniętych ikonek',//'撤销图标',
+
+	'REG'	=> 'Group recommend',//'群组推荐',
+
+	'PTA'	=> 'Wygenerowanych artykułów',//'生成文章',
+
+	'MAG'	=> 'Zużycie',//'道具',
+
+	'REB'	=> 'Promowanych elementów',//'推送',
+
+);
+
---
+++ upload/source/language/pl/task/lang_blog.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_blog.php by Valery Votintsev at sources.ru
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = array
+(
+	'blog_name'	=> 'Pierwszy wpis',//'发表日志任务',
+	'blog_desc'	=> 'Aby ukończyć to zadanie opublikuj swój pierwszy wpis na blogu, a otrzymasz dodatkowe punkty.',//'发表日志即可完成任务，获得相应的奖励',
+	'blog_view'	=> '<strong>Poniższe kroki pomogą Ci ukończyć to zadanie:</strong>
+		<ul>
+		<li>1. <a href="home.php?mod=spacecp&ac=blog" target="_blank">Kliknij tutaj by otworzyć stronę bloga</a>;</li>
+		<li>2. Stwórz pierwszy wpis i opublikuj go.</li>
+		</ul>',
+);
+
