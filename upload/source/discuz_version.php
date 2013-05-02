b170a6c1c93b4080b018cbe861c3e59536bb9432
vvotintsev <vvotintsev@gmail.com>
2013-05-02 10:25:33 +0400

    Upgrading 1 to RC 2013-04-22

---
+++ upload/source/discuz_version.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: discuz_version.php 33084 2013-04-19 01:37:08Z zhengqingpeng $
+ *	Modified by Valery Votintsev, codersclub.org
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+if(!defined('DISCUZ_VERSION')) {
+	define('DISCUZ_VERSION', 'X3 RC');
+	define('DISCUZ_RELEASE', '20130422');
+	define('DISCUZ_FIXBUG', '30000000');
+	define('DISCUZ_ML_REVISION', '91');
+}
+
+?>
---
+++ upload/source/language/pl/task/lang_gift.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *      [Discuz!] (C)2001-2099 Comsenz Inc.
+ *      This is NOT a freeware, use is subject to license terms
+ *
+ *      $Id: lang_gift.php by Valery Votintsev at sources.ru
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+$lang = array
+(
+	'gift_name'	=> 'Prezent',//'红包类任务',
+	'gift_desc'	=> 'Aby rozwiązać to zadanie, musisz otrzymać upominek od innej osoby.',//'申请此任务即可领取红包',
+);
+
